c5d88ba4c1f3429a6a21ee3b4f1795e76d5425a1
Jonathan <jonathan850629@hotmail.com>
2020-04-02 20:13:53 +0800

    '20200402'

---
+++ course/templatetags/tag_library.py
@@ -0,0 +1,18 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def get_filename(value):
+    return str(value).replace("files/","")
+@register.filter
+def to_int(value):
+    return int(value)
+@register.filter
+def to_str(value):
+    return str(value)
+@register.filter
+def youtube_url(value):
+    if value != '':
+        return 'https://www.youtube.com/watch?v='+value
+    return value

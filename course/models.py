7ebf4359901103731b29f072b45d7444c169eda4
leeshiawoei <41151929+leeshiawoei@users.noreply.github.com>
2020-06-22 20:58:00 +0800

    '20200622'

--- course/models.py
+++ course/models.py
@@ -3,6 +3,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Category(models.Model):
     category_name = models.CharField(max_length=255)
+    complier_url = models.TextField(null=True)
 
     def __str__(self):
         return self.category_name
@@ -119,6 +120,7 @@ class Review(models.Model):
     review = models.TextField(null=True,blank=True)
     rating = models.IntegerField(default=0,validators=[MinValueValidator(0), MaxValueValidator(5)])
     created_at = models.DateTimeField(auto_now_add=True)
+
 
 
 

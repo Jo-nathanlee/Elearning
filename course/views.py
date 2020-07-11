17213a7b037723c30a725f701bd83270de44b2d2
leeshiawoei <41151929+leeshiawoei@users.noreply.github.com>
2020-07-11 15:15:36 +0800

    '20200711_cdeditor_markdown'

--- course/views.py
+++ course/views.py
@@ -230,7 +230,7 @@ def course_edit(request,course_id):
 
     # updating course
     if request.method == "POST":
-        #try:
+        try:
             category_name = request.POST['category']
             model_category = models.Category.objects.get(category_name=category_name)  
             course.course_name = request.POST['course_name']
@@ -240,8 +240,8 @@ def course_edit(request,course_id):
             course.save()
             messages.add_message(request, messages.INFO, '編輯成功！')
             return HttpResponseRedirect('/course/edit/'+str(course_id)+'/')
-        #except Exception as e:
-            #messages.add_message(request, messages.ERROR, '編輯失敗！')
+        except Exception as e:
+            messages.add_message(request, messages.ERROR, '編輯失敗！')
     # showing edit page
     return render(request,'edit-course.html',locals())
 
@@ -330,7 +330,7 @@ def edit_lesson(request,course_id,lesson_id):
 
     #editing lesson
     if request.method == 'POST':
-        try:
+        #try:
             lesson.lesson_name = request.POST['lesson_name']
             lesson_video = request.POST['lesson_video']
             # 擷取youtube id
@@ -355,8 +355,8 @@ def edit_lesson(request,course_id,lesson_id):
             messages.add_message(request, messages.INFO, '編輯成功！')
             return HttpResponseRedirect('/course/edit/'+course_id+'/')
 
-        except Exception as e:
-            messages.add_message(request, messages.ERROR, '編輯失敗！') 
+        #except Exception as e:
+            #messages.add_message(request, messages.ERROR, '編輯失敗！') 
 
     # showing editing page
     return render(request,'edit-lesson.html',locals())

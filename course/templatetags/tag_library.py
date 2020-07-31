212d62d2a4c3a747f89722c8d21d1a9003ec9ea3
leeshiawoei <41151929+leeshiawoei@users.noreply.github.com>
2020-07-31 17:54:28 +0800

    '20200729group'

--- course/templatetags/tag_library.py
+++ course/templatetags/tag_library.py
@@ -55,7 +55,7 @@ def has_group(context):
         has_group = True
         my_group = Group.objects.filter(creator=request.user).first()
 
-    
+    returnlist = {'has_group': has_group,'group_id': my_group.id}
 
-    return {'has_group': has_group,'group_id': my_group.id}
+    return returnlist
 

ee03e10769515d436be5f66181d92d354bbcd072
leeshiawoei <41151929+leeshiawoei@users.noreply.github.com>
2020-07-31 19:00:57 +0800

    '20200729group'

--- course/templatetags/tag_library.py
+++ course/templatetags/tag_library.py
@@ -58,10 +58,10 @@ def has_group(context):
 
 @register.simple_tag( takes_context=True)
 def if_teacher(context):
+    teacher = None
     request = context['request']
     if_teacher = Course.objects.filter(teacher=request.user).count()
     if if_teacher>0:
-        return True
-    else:
-        return False
+        teacher = 'True'
+    return teacher
 

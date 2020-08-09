cfb8467bea6e78395e0718d37c31d8d7e6849e4f
leeshiawoei <41151929+leeshiawoei@users.noreply.github.com>
2020-08-09 22:19:36 +0800

    '20200809'

--- course/views.py
+++ course/views.py
@@ -621,16 +621,18 @@ def update_note(request):
     #try:
     lesson_id = request.POST['lesson_id']
     if_share = strtobool(request.POST['if_share'])
+    lesson = models.Lesson.objects.get(lesson_id=lesson_id)
+
 
     if 'note' in request.POST:
         note = request.POST['note']
         models.Note.objects.update_or_create(
-            lesson=lesson_id, student=request.user.id,
+            lesson=lesson, student=request.user,
             defaults={'note': note,'if_share':if_share},
         )
     else:
         models.Note.objects.update_or_create(
-            lesson=lesson_id, student=request.user.id,
+            lesson=lesson, student=request.user,
             defaults={'if_share':if_share},
         )
     data = {}

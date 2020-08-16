de6628ed140ac11153457cef578d3108f7120a37
leeshiawoei <41151929+leeshiawoei@users.noreply.github.com>
2020-08-16 21:09:21 +0800

    '20200816'

--- course/views.py
+++ course/views.py
@@ -423,7 +423,7 @@ def lesson_page(request,lesson_id,lesson_index):
             #all_homework = models.Homework.objects.filter(lesson=lesson)
             students =  models.UserCourse.objects.filter(course=course).values('user__id', 'user__name','user__pic') 
             for student in students:
-                homework = models.Homework.objects.filter(lesson=lesson,student=student).values('homework','homework_url', 'updated_at')
+                homework = models.Homework.objects.filter(lesson=lesson,student=student['user__id']).values('homework','homework_url', 'updated_at')
                 student['homework'] = list(homework)  
             all_homework = list(students)
         else:
